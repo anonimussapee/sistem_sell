070d1cd522cecdecb8870213be17253956692910
jdx <librededrogas420@gmail.com>
2023-07-10 12:36:27 -0500

    i added justify ecneter in layout

--- src/Componets/Layout/index.jsx
+++ src/Componets/Layout/index.jsx
@@ -1,6 +1,6 @@
 const Layout = (props) => {
   return (
-    <section className='relative w-[100%] h-[100vh] flex flex-col items-center gap-5 py-[100px] bg-gray-100 '>
+    <section className='relative w-[100%] h-[100vh] flex flex-col items-center justify-center gap-5 py-[100px] bg-gray-100 '>
       {props.children}
     </section>
   )
